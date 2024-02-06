e2a19a3ce131b54512660b82ce57b2ee866283bd
Luke Poirrier <lukepoirrier5@gmail.com>
2024-02-06 07:03:47 -0500

    Practice nested for loops and arrays

---
+++ ArraysAndLoops.java
@@ -0,0 +1,19 @@
+
+public class ArraysAndLoops implements forLoop {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int i = 0; i < numbers.length; i++) {
+            
+            System.out.printf("|%-3d|", numbers[i]);
+            System.out.printf("|%-3d|", numbers[i]);
+            System.out.printf("|%-3d|", numbers[i]);
+            System.out.printf("|%-3d|", numbers[i]);
+            System.out.printf("|%-3d|", numbers[i]);
+
+            for(int j = 0; j < numbers.length; j++){
+                System.out.printf("------%s\n", "------");
+                System.out.printf("-%-8d-", numbers[j]);
+            }
+        }
+    }
+}
