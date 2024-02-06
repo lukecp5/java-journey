02c553f21a50331a92f9a34e0e2ccb816e07c250
Luke Poirrier <lukepoirrier5@gmail.com>
2024-02-06 07:03:05 -0500

    Practice array methods

---
+++ arrayMethods.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class arrayMethods {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i));
+        }
+        System.out.println(numbers.);
+    }
+}
