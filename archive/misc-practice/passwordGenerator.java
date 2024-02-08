e97f8976f8959abebbf331e3b33311846367ab82
Luke Poirrier <lukepoirrier5@gmail.com>
2024-02-07 23:45:49 -0500

    Move older Files to archive

--- archive/misc-practice/passwordGenerator.java
+++ archive/misc-practice/passwordGenerator.java
@@ -10,17 +10,18 @@ public class passwordGenerator {
 
         System.out.print("Do you want to include symbols? (Y/N): ");
         boolean includeSymbols = scanner.next().equalsIgnoreCase("Y");
-
+        scanner.close();
         String password = generatePassword(length, includeSymbols);
         System.out.println("Generated password: " + password);
     }
 
     private static String generatePassword(int length, boolean includeSymbols) {
         /// A string of all possible characters that could be included in the password.
-        //  Contains all uppercase and lowercase letters, and digits from 0 to 9. */
+        // Contains all uppercase and lowercase letters, and digits from 0 to 9. */
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-        // NOTE: If 'includeSymbols' flag is true, then special symbol characters are added to the 'characters' string.
+        // NOTE: If 'includeSymbols' flag is true, then special symbol characters are
+        // added to the 'characters' string.
         if (includeSymbols) {
             characters += "!@#$%^&*()";
         }
@@ -31,13 +32,16 @@ public class passwordGenerator {
         // A StringBuilder object is created to build the password string.
         StringBuilder password = new StringBuilder();
 
-        // A for loop runs 'length' times, where 'length' is the desired length of the password.
+        // A for loop runs 'length' times, where 'length' is the desired length of the
+        // password.
         for (int i = 0; i < length; i++) {
 
-            // A random index is generated within the range of the 'characters' string length.
+            // A random index is generated within the range of the 'characters' string
+            // length.
             int index = random.nextInt(characters.length());
 
-            // Character at the randomly generated index is appended to the 'password' string.
+            // Character at the randomly generated index is appended to the 'password'
+            // string.
             password.append(characters.charAt(index));
         }
 
