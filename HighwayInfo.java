f9a9572187b4faaad68126f95019225a0e129fa1
Luke Poirrier <lukepoirrier5@gmail.com>
2024-02-07 22:46:10 -0500

    Move files to correct Directories and start on Highway # project

---
+++ HighwayInfo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class HighwayInfo {
+    public static void main(String[] args) {
+        // declare and instantiate a Scanner
+        Scanner in = new Scanner(System.in);
+
+    //Info:    Enter the highway number : 185
+    //Info:    The 185 is auxiliary, serving the 85, going north/south.
+
+
+
+        // declare and initialize any other needed variables
+        int usrHwy = 0;
+        int hwyNum = 0;
+        boolean aux = false;
+        String dir = "";
+
+
+        // * Prompt user for highway number and store in usrHwy *
+        System.out.println("Enter the highway number : ");
+        usrHwy = in.nextInt();
+
+        if(usrHwy < 0){
+            
+        }
+
+        
+    }
+}
---
+++ hwy-info.md
@@ -0,0 +1,16 @@
+You can use the modulus operator (%) to compute the reminder of an integer division operation.
+
+For example: 290 % 100 will evaluate to 90, because the remainder after dividing 290 by 100 is 90.
+
+As another example: 185 % 2 is 1, because 2 goes into 185, 92 times with 1 left over. Also, note that for any integer x, x % 2 will always evaluate to 1 when x is odd, and it will always evaluate to 0 when x is even.
+
+Example 2:
+
+Enter the highway number : 290
+
+The 290 is auxiliary, serving the 90, going east/west.
+Example 3:
+
+Enter the highway number : 185
+
+The 185 is auxiliary, serving the 85, going north/south.
