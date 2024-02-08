fd848febc1836d853be552f17e649f280c46e091
Luke Poirrier <lukepoirrier5@gmail.com>
2024-02-07 23:22:35 -0500

    Finish writing HighwayInfo and test

--- HighwayInfo.java
+++ HighwayInfo.java
@@ -5,26 +5,32 @@ public class HighwayInfo {
         // declare and instantiate a Scanner
         Scanner in = new Scanner(System.in);
 
-    //Info:    Enter the highway number : 185
-    //Info:    The 185 is auxiliary, serving the 85, going north/south.
-
-
-
-        // declare and initialize any other needed variables
+        // Declare variables needed
         int usrHwy = 0;
         int hwyNum = 0;
-        boolean aux = false;
+        String aux = "";
         String dir = "";
-
+        String NS = " north/south.";
+        String EW = " east/west.";
 
         // * Prompt user for highway number and store in usrHwy *
         System.out.println("Enter the highway number : ");
         usrHwy = in.nextInt();
 
-        if(usrHwy < 0){
-            
+        // Conditional statements to determine direction and actual highway number
+        if (usrHwy % 2 == 0) {
+            dir = EW;
+        } else {
+            dir = NS;
         }
-
-        
+        if (usrHwy > 100) {
+            aux = "auxilliary, serving the";
+            hwyNum = usrHwy % 100;
+            System.out.printf("The %d is %s %d, going %s", usrHwy, aux, hwyNum, dir);
+        } else {
+            aux = "primary, going ";
+            hwyNum = usrHwy;
+            System.out.printf("The %d is %s%s", hwyNum, aux, dir);
+        }
     }
 }
