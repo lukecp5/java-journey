034837ba957463f3749dac4e1407630127459211
Luke Poirrier <lukepoirrier5@gmail.com>
2024-03-06 22:33:45 -0500

    Java Object Practice

---
+++ OOP/serverInstantiation.java
@@ -0,0 +1,13 @@
+package OOP;
+
+// Import the Server class from the correct location
+import OOP.Server;
+
+public class serverInstantiation{
+    public static void main(String[] args) {
+        Server server = new Server(32, "Intel Xeon", 500);
+        System.out.printf("Server RAM: %-16f\n", server.ram);
+        System.out.printf("Server CPU: %-16s\n", server.cpu);
+        System.out.printf("Server Storage: %-16f\n", server.storage);
+    }
+}
